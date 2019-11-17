e3e61875d2ae63089d39e0ebed7840c60a64d32a
nayantaka <nayantaka@gmail.com>
2019-11-17 11:48:21 +0700

    Update dasar.php

--- dasar.php
+++ dasar.php
@@ -16,7 +16,7 @@ var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 </script>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZ35SNIok6htTXFQ_EGrO8lVQxyvYMAKA&callback=myMap"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=APIK_key&callback=myMap"></script>
 
 </body>
 </html>
