7bd2100370b62c743a436bae23dd0687e7b70d19
nayantaka <nayantaka@gmail.com>
2019-11-17 09:38:47 +0700

    Add files via upload

---
+++ dasar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Map Semarang</h1>
+
+<div id="googleMap" style="width:600px;height:600px;"></div>
+
+<script>
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(-6.9923263,110.4288568),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZ35SNIok6htTXFQ_EGrO8lVQxyvYMAKA&callback=myMap"></script>
+
+</body>
+</html>
